0d6f1eb6fb9cb000a3f1303b1f2526ea7360a682
Keshav <kmalik0907@gmail.com>
2023-04-26 02:33:50 +0530

    first

---
+++ src/Data/QuizData.js
@@ -0,0 +1,52 @@
+export const QuizData = [
+    {
+        question: "Which of the following command is used to create react-js-app ?",
+        options:[ "npx create-react-app appname", "npm install create-react-app", "npx install create-react-app -g", "	install - l create-react-app"],
+        answer: 1
+    },
+    {
+        question: "In React.js which one of the following is used to create a class for Inheritance ?",
+        options:["Create", "Extends", "	Inherits", "Delete"],
+        answer: 2
+    },
+    {
+        question: "What is the default port number in which the application run ?",
+        options: ["8000", "8080", "3000", "5000"],
+        answer: 3
+    },
+    {
+        question: "Which of the following valid component return type of React ?",
+        options: ["2", "5", "1", "3"],
+        answer: 3
+    },
+    {
+        question: "Which of the following is must for the API in React.js ?",
+        options: ["SetinitialComponent", "renderComponent", "render", "All of the above"],
+        answer: 2
+    },
+    {
+        question: "Which of the following is true regarding Babel ?",
+        options: ["Compiler", "Transpilar", "Both of the above", "None of the above"],
+        answer:3
+    },
+    {
+        question: "In React.js, how we can pass the data from one component to another in React.js?",
+        options: ["SetState", "Render with arguments", "Props", "PropTypes"],
+        answer:3
+    },
+    {
+        question: "Which of the following function is true about changing the state in React.js ? ",
+        options: ["this.State{}", "this.setState", "this.setChangeState", "All of the above"],
+        answer:2
+    },
+    {
+        question: "Which of the following method is true about referring parent class in React.js ?",
+        options: ["self()", "inherits()", "this()", "super()"],
+        answer:4
+    },
+    {
+        question: "Which of the following is a way to handle data in React.js ?",
+        options: ["State & Props", "Services & Components", "State & Services", "State & Component"],
+        answer:4
+    },
+];
